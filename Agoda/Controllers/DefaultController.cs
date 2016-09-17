dfe685960ac0e3ea949addada4a1d4dc2832d03e
neowind <eakkaau@gmail.com>
2016-09-17 17:27:56 +0700

    first

---
+++ Agoda/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using Agoda.Models;
+
+namespace Agoda.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private HotelDBContext db = new HotelDBContext();
+
+        //
+        // GET: /Default/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Static()
+        {
+            return View(db.Hotels.ToList());
+        }
+
+        public ActionResult Ajax()
+        {
+            return View();
+        }
+    }
+}
---
+++ Agoda/Controllers/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agoda.Controllers
+{
+    public class Home : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
